35fcd6f9776e2f6da8c2e7d6fa6ef5ec0833e187
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 14:30:53 +0100

    Update auth_event.dart

--- lib/features/auth/bloc/auth_event.dart
+++ lib/features/auth/bloc/auth_event.dart
@@ -1,4 +1,44 @@
 part of 'auth_bloc.dart';
 
 @immutable
-sealed class AuthEvent {}
+abstract class AuthEvent {}
+
+class LoginRequested extends AuthEvent {
+  final String email;
+  final String password;
+  LoginRequested(this.email, this.password);
+}
+
+class RegisterRequested extends AuthEvent {
+  final Map<String, dynamic> userData;
+  RegisterRequested(this.userData);
+}
+
+class VerifyOtpRequested extends AuthEvent {
+  final String email;
+  final String otp;
+  final bool isFromRegister;
+  VerifyOtpRequested({
+    required this.email,
+    required this.otp,
+    required this.isFromRegister,
+  });
+}
+
+class ResendOtpRequested extends AuthEvent {
+  final String email;
+  ResendOtpRequested(this.email);
+}
+
+class ForgotPasswordRequested extends AuthEvent {
+  final String email;
+  ForgotPasswordRequested(this.email);
+}
+
+class ResetPasswordRequested extends AuthEvent {
+  final String password;
+  final String passwordConfirmation;
+  ResetPasswordRequested(this.password, this.passwordConfirmation);
+}
+
+class LogoutRequested extends AuthEvent {}
