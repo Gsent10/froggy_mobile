fd3342350322cafecac45ec5f38014aff1bbf722
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 23:15:19 +0100

    Update loading.dart

--- lib/core/widgets/loading.dart
+++ lib/core/widgets/loading.dart
@@ -9,7 +9,7 @@ class Loading extends StatelessWidget {
     return Container(
       width: context.screenWidth,
       height: context.screenHeight,
-      decoration: BoxDecoration(color: kBlackColor.withOpacity(0.1)),
+      decoration: BoxDecoration(color: kBlackColor.withOpacity(0.5)),
       child: Center(child: Image.asset('assets/loading.gif', width: 100)),
     );
   }
