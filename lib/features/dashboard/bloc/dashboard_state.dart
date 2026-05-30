4f8b3edaeba13db94b4cf8a9c7e56708351faef9
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-31 00:54:34 +0100

    Create dashboard_state.dart

---
+++ lib/features/dashboard/bloc/dashboard_state.dart
@@ -0,0 +1,13 @@
+part of 'dashboard_bloc.dart';
+
+class DashboardState {
+  final int currentIndex;
+
+  DashboardState({this.currentIndex = 0});
+
+  DashboardState copyWith({int? currentIndex}) {
+    return DashboardState(
+      currentIndex: currentIndex ?? this.currentIndex,
+    );
+  }
+}
