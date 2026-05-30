e0e055069b45098b6dc312181deee21640ac8873
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 14:31:05 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:froggy_mobile/core/utils/user_simple_preferences.dart';
+import 'package:froggy_mobile/features/auth/bloc/auth_bloc.dart';
+import 'package:froggy_mobile/features/auth/presentation/pages/forgot_password_page.dart';
+import 'package:froggy_mobile/features/auth/presentation/pages/login_page.dart';
+import 'package:froggy_mobile/features/auth/presentation/pages/new_password_page.dart';
+import 'package:froggy_mobile/features/auth/presentation/pages/register_page.dart';
+import 'package:froggy_mobile/features/auth/presentation/pages/verify_otp_page.dart';
+import 'package:froggy_mobile/features/dashboard/presentation/pages/dashboard_page.dart';
 import 'package:froggy_mobile/features/launch/bloc/launch_bloc.dart';
 import 'package:froggy_mobile/features/launch/presentation/pages/launch_screen.dart';
 
@@ -15,16 +22,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => LaunchBloc()..add(CheckLaunchStatus()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LaunchBloc()..add(CheckLaunchStatus()),
+        ),
+        BlocProvider(create: (context) => AuthBloc()),
+      ],
       child: MaterialApp(
         title: 'Froggy Mobile',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff5D5FEF)),
           useMaterial3: true,
         ),
         home: const LaunchScreen(),
-        debugShowCheckedModeBanner: false,
+        routes: {
+          '/login': (context) => const LoginPage(),
+          '/register': (context) => const RegisterPage(),
+          '/verify-otp': (context) => const VerifyOtpPage(),
+          '/forgot-password': (context) => const ForgotPasswordPage(),
+          '/new-password': (context) => const NewPasswordPage(),
+          '/dashboard': (context) => const DashboardPage(),
+        },
       ),
     );
   }
