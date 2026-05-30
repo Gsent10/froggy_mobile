495f9b586066b4b7cc2966f91371c86ec26da81b
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 23:05:13 +0100

    Create loading.dart

---
+++ lib/core/widgets/loading.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:froggy_mobile/core/utils/utils.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: context.screenWidth,
+      height: context.screenHeight,
+      decoration: BoxDecoration(color: kBlackColor.withOpacity(0.1)),
+      child: Center(child: Image.asset('assets/loading.gif', width: 100)),
+    );
+  }
+}
