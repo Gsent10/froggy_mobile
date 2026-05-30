a7d072bb13d80437d460e5add1bbfe329e13532c
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-31 00:48:22 +0100

    Update dashboard_page.dart

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:froggy_mobile/core/utils/utils.dart';
 import 'package:froggy_mobile/features/auth/bloc/auth_bloc.dart';
+import 'package:froggy_mobile/features/dashboard/presentation/widgets/home_page.dart';
+import 'package:froggy_mobile/features/dashboard/presentation/widgets/rates_page.dart';
+import 'package:froggy_mobile/features/dashboard/presentation/widgets/settings_page.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const RatesPage(),
+    const SettingsPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +31,43 @@ class DashboardPage extends StatelessWidget {
           IconButton(
             onPressed: () {
               context.read<AuthBloc>().add(LogoutRequested());
-              Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false);
+              Navigator.of(
+                context,
+              ).pushNamedAndRemoveUntil('/login', (route) => false);
             },
             icon: const Icon(Icons.logout),
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Welcome to Froggy Mobile Dashboard!'),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          backgroundColor: kWhiteColor,
+          selectedItemColor: kPrimaryColor,
+          unselectedItemColor: Colors.grey,
+          elevation: 5,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 30),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_chart, size: 30),
+              label: 'Rates',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings, size: 30),
+              label: 'Settings',
+            ),
+          ],
+        ),
       ),
     );
   }
