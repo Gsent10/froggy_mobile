f5f765e86bcf55cf221e2db32ce5cb484374219f
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 23:20:26 +0100

    Update new_password_page.dart

--- lib/features/auth/presentation/pages/new_password_page.dart
+++ lib/features/auth/presentation/pages/new_password_page.dart
@@ -31,10 +31,6 @@ class _NewPasswordPageState extends State<NewPasswordPage> {
           Navigator.of(
             context,
           ).pushNamedAndRemoveUntil('/login', (route) => false);
-        } else if (state.status == AuthStatus.error) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(state.errorMessage ?? 'An error occurred')),
-          );
         }
       },
       child: Scaffold(
