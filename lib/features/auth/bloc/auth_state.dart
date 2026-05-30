631d15e794de31ee0933124ae7727cd791e3d215
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 14:30:56 +0100

    Update auth_state.dart

--- lib/features/auth/bloc/auth_state.dart
+++ lib/features/auth/bloc/auth_state.dart
@@ -1,6 +1,42 @@
 part of 'auth_bloc.dart';
 
-@immutable
-sealed class AuthState {}
+enum AuthStatus {
+  initial,
+  loading,
+  authenticated,
+  unauthenticated,
+  otpRequired,
+  otpVerified,
+  error
+}
 
-final class AuthInitial extends AuthState {}
+@immutable
+class AuthState {
+  final AuthStatus status;
+  final String? errorMessage;
+  final String? email; // To persist email during OTP flow
+  final bool isFromRegister;
+
+  AuthState({
+    this.status = AuthStatus.initial,
+    this.errorMessage,
+    this.email,
+    this.isFromRegister = false,
+  });
+
+  AuthState copyWith({
+    AuthStatus? status,
+    String? errorMessage,
+    String? email,
+    bool? isFromRegister,
+  }) {
+    return AuthState(
+      status: status ?? this.status,
+      errorMessage: errorMessage ?? this.errorMessage,
+      email: email ?? this.email,
+      isFromRegister: isFromRegister ?? this.isFromRegister,
+    );
+  }
+}
+
+class AuthInitial extends AuthState {}
