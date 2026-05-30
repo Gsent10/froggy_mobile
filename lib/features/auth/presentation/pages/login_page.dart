8dbfb40d85935a70ff668aa313220ab4ce9a073b
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 23:14:26 +0100

    Update login_page.dart

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:froggy_mobile/core/utils/utils.dart';
 import 'package:froggy_mobile/core/widgets/button.dart';
 import 'package:froggy_mobile/core/widgets/form_input.dart';
+import 'package:froggy_mobile/core/widgets/loading.dart';
 import 'package:froggy_mobile/features/auth/bloc/auth_bloc.dart';
 import 'package:froggy_mobile/features/auth/presentation/widgets/auth_header.dart';
 
@@ -34,127 +35,131 @@ class _LoginPageState extends State<LoginPage> {
       },
       child: Scaffold(
         backgroundColor: kBgColor,
-        body: Container(
-          width: context.screenWidth,
-          height: context.screenHeight,
-          padding: EdgeInsets.symmetric(
-            horizontal: context.screenWidth * kHorizontalPadding,
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                AuthHeader(
-                  label: 'Login',
-                  subtitle: 'Welcome back! Please login to your account',
-                  isFirstPage: widget.isFirstPage,
-                ),
-
-                // Form Items
-                FormInput(
-                  textEditingController: emailController,
-                  title: 'Email Address',
-                  label: 'Enter your email',
-                  mail: true,
-                ),
-                FormInput(
-                  textEditingController: passwordController,
-                  title: 'Password',
-                  label: 'Enter your password',
-                  pass: true,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+        body: Stack(
+          children: [
+            Container(
+              width: context.screenWidth,
+              height: context.screenHeight,
+              padding: EdgeInsets.symmetric(
+                horizontal: context.screenWidth * kHorizontalPadding,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed('/forgot-password');
-                      },
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.zero,
-                        minimumSize: Size.zero,
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      ),
-                      child: Text(
-                        'Forgot Password?',
-                        style: SafeGoogleFont(
-                          'Ubuntu',
-                          fontSize: context.screenWidth * kFontS,
-                          fontWeight: FontWeight.w600,
-                          color: kPrimaryColor,
-                          decoration: TextDecoration.none,
-                        ),
-                      ),
+                    AuthHeader(
+                      label: 'Login',
+                      subtitle: 'Welcome back! Please login to your account',
+                      isFirstPage: widget.isFirstPage,
                     ),
-                  ],
-                ),
 
-                SizedBox(height: context.screenHeight * kSpacingM),
-
-                // Submit Button
-                BlocBuilder<AuthBloc, AuthState>(
-                  builder: (context, state) {
-                    return GestureDetector(
-                      onTap: state.status == AuthStatus.loading
-                          ? null
-                          : () {
-                              context.read<AuthBloc>().add(
-                                LoginRequested(
-                                  emailController.text,
-                                  passwordController.text,
-                                ),
-                              );
-                            },
-                      child: state.status == AuthStatus.loading
-                          ? const CircularProgressIndicator()
-                          : const Button(label: 'Login'),
-                    );
-                  },
-                ),
-
-                SizedBox(height: context.screenHeight * kSpacingL),
-
-                if (widget.isFirstPage)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        "Don't have an Account? ",
-                        style: SafeGoogleFont(
-                          'Ubuntu',
-                          fontSize: context.screenWidth * kFontS,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pushNamed('/register');
-                        },
-                        style: TextButton.styleFrom(
-                          padding: EdgeInsets.zero,
-                          minimumSize: Size.zero,
-                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        ),
-                        child: Text(
-                          "Sign up here",
-                          style: SafeGoogleFont(
-                            'Ubuntu',
-                            fontSize: context.screenWidth * kFontS,
-                            fontWeight: FontWeight.w600,
-                            color: kPrimaryColor,
+                    // Form Items
+                    FormInput(
+                      textEditingController: emailController,
+                      title: 'Email Address',
+                      label: 'Enter your email',
+                      mail: true,
+                    ),
+                    FormInput(
+                      textEditingController: passwordController,
+                      title: 'Password',
+                      label: 'Enter your password',
+                      pass: true,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/forgot-password');
+                          },
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            minimumSize: Size.zero,
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          ),
+                          child: Text(
+                            'Forgot Password?',
+                            style: SafeGoogleFont(
+                              'Ubuntu',
+                              fontSize: context.screenWidth * kFontS,
+                              fontWeight: FontWeight.w600,
+                              color: kPrimaryColor,
+                              decoration: TextDecoration.none,
+                            ),
                           ),
                         ),
+                      ],
+                    ),
+
+                    SizedBox(height: context.screenHeight * kSpacingM),
+
+                    // Submit Button
+                    GestureDetector(
+                      onTap: () {
+                        context.read<AuthBloc>().add(
+                          LoginRequested(
+                            emailController.text,
+                            passwordController.text,
+                          ),
+                        );
+                      },
+                      child: const Button(label: 'Login'),
+                    ),
+
+                    SizedBox(height: context.screenHeight * kSpacingL),
+
+                    if (widget.isFirstPage)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "Don't have an Account? ",
+                            style: SafeGoogleFont(
+                              'Ubuntu',
+                              fontSize: context.screenWidth * kFontS,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pushNamed('/register');
+                            },
+                            style: TextButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              minimumSize: Size.zero,
+                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            ),
+                            child: Text(
+                              "Sign up here",
+                              style: SafeGoogleFont(
+                                'Ubuntu',
+                                fontSize: context.screenWidth * kFontS,
+                                fontWeight: FontWeight.w600,
+                                color: kPrimaryColor,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-              ],
+                  ],
+                ),
+              ),
             ),
-          ),
+            BlocBuilder<AuthBloc, AuthState>(
+              builder: (context, state) {
+                if (state.status == AuthStatus.loading) {
+                  return const Loading();
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          ],
         ),
       ),
     );
