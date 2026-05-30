80b3477ad12d14baa1d85c4dc08327fb803ad692
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 13:02:52 +0100

    Update user_simple_preferences.dart

--- lib/core/utils/user_simple_preferences.dart
+++ lib/core/utils/user_simple_preferences.dart
@@ -6,6 +6,11 @@ class UserSimplePreferences {
   static Future init() async =>
       _preferences = await SharedPreferences.getInstance();
 
+  static Future setLaunched(bool launched) async =>
+      await _preferences?.setBool('launched', launched);
+
+  static bool? getLaunched() => _preferences?.getBool('launched');
+
   static Future setToken(String token) async =>
       await _preferences?.setString('token', token);
 
