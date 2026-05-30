acd0d161d05b49de9deb9e048f701fc96c53ed67
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 13:36:14 +0100

    Update login_page.dart

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:froggy_mobile/core/utils/utils.dart';
+import 'package:froggy_mobile/features/auth/presentation/widgets/auth_header.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+    return Scaffold(
+      body: Container(
+        width: context.screenWidth,
+        height: context.screenHeight,
+        padding: EdgeInsets.symmetric(
+          horizontal: context.screenWidth * kHorizontalPadding,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AuthHeader(),
+
+              // Form Items
+            ],
+          ),
+        ),
       ),
     );
   }
