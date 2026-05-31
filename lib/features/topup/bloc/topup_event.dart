e7b65691db703d86c940eec400d68bdd6dc55114
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-31 12:42:30 +0100

    Update topup_event.dart

--- lib/features/topup/bloc/topup_event.dart
+++ lib/features/topup/bloc/topup_event.dart
@@ -1,4 +1,19 @@
 part of 'topup_bloc.dart';
 
-@immutable
-sealed class TopupEvent {}
+abstract class TopupEvent {}
+
+class SubmitTopup extends TopupEvent {
+  final String currencyCode;
+  final double amount;
+  final String paymentMethod;
+  final String idempotencyKey;
+
+  SubmitTopup({
+    required this.currencyCode,
+    required this.amount,
+    required this.paymentMethod,
+    required this.idempotencyKey,
+  });
+}
+
+class ResetTopup extends TopupEvent {}
