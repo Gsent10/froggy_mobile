06b6390b4cfcb16e9fa84db17403dabfc05f7c6c
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-31 14:02:45 +0100

    Update transfer_state.dart

--- lib/features/transfer/bloc/transfer_state.dart
+++ lib/features/transfer/bloc/transfer_state.dart
@@ -1,6 +1,31 @@
 part of 'transfer_bloc.dart';
 
-@immutable
-sealed class TransferState {}
+enum TransferStatus { initial, loading, success, failed, error }
 
-final class TransferInitial extends TransferState {}
+class TransferState {
+  final TransferStatus status;
+  final String? transactionReference;
+  final String? errorMessage;
+  final Wallet? updatedFromWallet;
+
+  const TransferState({
+    this.status = TransferStatus.initial,
+    this.transactionReference,
+    this.errorMessage,
+    this.updatedFromWallet,
+  });
+
+  TransferState copyWith({
+    TransferStatus? status,
+    String? transactionReference,
+    String? errorMessage,
+    Wallet? updatedFromWallet,
+  }) {
+    return TransferState(
+      status: status ?? this.status,
+      transactionReference: transactionReference ?? this.transactionReference,
+      errorMessage: errorMessage ?? this.errorMessage,
+      updatedFromWallet: updatedFromWallet ?? this.updatedFromWallet,
+    );
+  }
+}
