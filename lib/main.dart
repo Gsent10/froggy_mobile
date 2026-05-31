4fbb5483e2144ed121a9316a40d09da8fc44f941
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-31 10:38:30 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,12 @@ import 'package:froggy_mobile/features/auth/presentation/pages/verify_otp_page.d
 import 'package:froggy_mobile/features/dashboard/bloc/dashboard_bloc.dart';
 import 'package:froggy_mobile/features/wallet/bloc/wallet_bloc.dart';
 import 'package:froggy_mobile/features/dashboard/presentation/pages/dashboard_page.dart';
+import 'package:froggy_mobile/features/topup/presentation/pages/topup_page.dart';
+import 'package:froggy_mobile/features/transfer/presentation/pages/transfer_page.dart';
 import 'package:froggy_mobile/features/wallet/presentation/pages/wallet_details_page.dart';
+import 'package:froggy_mobile/features/wallet/presentation/pages/wallet_list_page.dart';
+import 'package:froggy_mobile/features/wallet/presentation/pages/add_wallet_page.dart';
+import 'package:froggy_mobile/features/withdraw/presentation/pages/withdraw_page.dart';
 import 'package:froggy_mobile/features/launch/bloc/launch_bloc.dart';
 import 'package:froggy_mobile/features/launch/presentation/pages/launch_screen.dart';
 
@@ -64,6 +69,11 @@ class MyApp extends StatelessWidget {
           '/forgot-password': (context) => const ForgotPasswordPage(),
           '/new-password': (context) => const NewPasswordPage(),
           '/dashboard': (context) => const DashboardPage(),
+          '/wallet-list': (context) => const WalletListPage(),
+          '/transfer': (context) => const TransferPage(),
+          '/topup': (context) => const TopupPage(),
+          '/withdraw': (context) => const WithdrawPage(),
+          '/add-wallet': (context) => const AddWalletPage(),
         },
       ),
     );
