4afc2cace417ce9ca5818d51dc0b368e70141627
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-31 12:42:33 +0100

    Update topup_state.dart

--- lib/features/topup/bloc/topup_state.dart
+++ lib/features/topup/bloc/topup_state.dart
@@ -1,6 +1,31 @@
 part of 'topup_bloc.dart';
 
-@immutable
-sealed class TopupState {}
+enum TopupStatus { initial, loading, success, failed, error }
 
-final class TopupInitial extends TopupState {}
+class TopupState {
+  final TopupStatus status;
+  final String? errorMessage;
+  final Wallet? updatedWallet;
+  final String? transactionReference;
+
+  const TopupState({
+    this.status = TopupStatus.initial,
+    this.errorMessage,
+    this.updatedWallet,
+    this.transactionReference,
+  });
+
+  TopupState copyWith({
+    TopupStatus? status,
+    String? errorMessage,
+    Wallet? updatedWallet,
+    String? transactionReference,
+  }) {
+    return TopupState(
+      status: status ?? this.status,
+      errorMessage: errorMessage ?? this.errorMessage,
+      updatedWallet: updatedWallet ?? this.updatedWallet,
+      transactionReference: transactionReference ?? this.transactionReference,
+    );
+  }
+}
