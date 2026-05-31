cbbc16b5c9bf388806c9ebe46ef3f13094dc8be7
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-31 09:39:33 +0100

    Update dashboard_page.dart

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:froggy_mobile/core/utils/utils.dart';
+import 'package:froggy_mobile/core/widgets/loading.dart';
 import 'package:froggy_mobile/features/dashboard/bloc/dashboard_bloc.dart';
 import 'package:froggy_mobile/features/dashboard/presentation/widgets/home_page.dart';
 import 'package:froggy_mobile/features/dashboard/presentation/widgets/rates_page.dart';
@@ -15,57 +16,62 @@ class DashboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<DashboardBloc, DashboardState>(
       builder: (context, state) {
-        return Scaffold(
-          body: _pages[state.currentIndex],
-          bottomNavigationBar: Container(
-            height: context.screenHeight * 0.09,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 5,
-                  spreadRadius: 1,
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.vertical(
-                top: Radius.circular(30),
-              ),
-              child: Theme(
-                data: Theme.of(context).copyWith(
-                  splashColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                ),
-                child: BottomNavigationBar(
-                  currentIndex: state.currentIndex,
-                  onTap: (index) {
-                    context.read<DashboardBloc>().add(TabChanged(index));
-                  },
-                  backgroundColor: kWhiteColor,
-                  selectedItemColor: kPrimaryColor,
-                  unselectedItemColor: Colors.grey,
-                  type: BottomNavigationBarType.fixed,
-                  elevation: 0,
-                  items: const [
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.home, size: 30),
-                      label: 'Home',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.add_chart, size: 30),
-                      label: 'Rates',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.settings, size: 30),
-                      label: 'Settings',
+        return Stack(
+          children: [
+            Scaffold(
+              body: _pages[state.currentIndex],
+              bottomNavigationBar: Container(
+                height: context.screenHeight * 0.08,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 5,
+                      spreadRadius: 1,
                     ),
                   ],
                 ),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(30),
+                  ),
+                  child: Theme(
+                    data: Theme.of(context).copyWith(
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                    ),
+                    child: BottomNavigationBar(
+                      currentIndex: state.currentIndex,
+                      onTap: (index) {
+                        context.read<DashboardBloc>().add(TabChanged(index));
+                      },
+                      backgroundColor: kWhiteColor,
+                      selectedItemColor: kPrimaryColor,
+                      unselectedItemColor: Colors.grey,
+                      type: BottomNavigationBarType.fixed,
+                      elevation: 0,
+                      items: const [
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home, size: 30),
+                          label: 'Home',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.add_chart, size: 30),
+                          label: 'Rates',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.settings, size: 30),
+                          label: 'Settings',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
+            if (state.status == DashboardStatus.loading) const Loading(),
+          ],
         );
       },
     );
