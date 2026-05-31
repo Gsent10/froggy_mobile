5ef8cf51e2a1010d469fd4a0119b583a12fb7ceb
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-31 11:53:51 +0100

    Update wallet_event.dart

--- lib/features/wallet/bloc/wallet_event.dart
+++ lib/features/wallet/bloc/wallet_event.dart
@@ -6,3 +6,10 @@ class FetchWalletDetails extends WalletEvent {
   final int walletId;
   FetchWalletDetails(this.walletId);
 }
+
+class CreateWallet extends WalletEvent {
+  final String currencyCode;
+  CreateWallet(this.currencyCode);
+}
+
+class ResetWalletCreateStatus extends WalletEvent {}
